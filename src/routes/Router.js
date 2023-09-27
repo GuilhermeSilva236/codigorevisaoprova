4a099ac9ebf06d6023fdda00a9d53b2ff38e88b4
GuilhermeSilva236 <ggui2561@gmail.com>
2023-09-27 13:48:28 -0300

    first commit

---
+++ src/routes/Router.js
@@ -0,0 +1,26 @@
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import React from 'react'
+import Home from '../screens/home/Home'
+import Posts from '../screens/posts/Posts'
+import Usuario from '../screens/usuario/Ususario'
+
+// ROTAS
+
+const Stack = createStackNavigator()
+
+export default function Router() {
+    return (
+
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName='Home'>
+
+                <Stack.Screen name='Home' component={Home} />
+                <Stack.Screen name='Usuário' component={Usuario} />
+                <Stack.Screen name='Posts' component={Posts} />
+
+            </Stack.Navigator>
+        </NavigationContainer>
+
+    )
+}
